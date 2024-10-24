a579143a9a3a4b910d538e36791bc2c72ed35ccb
MeRay <git@meray.org>
2024-10-24 21:07:42 +0200

    cleaned up raytop configuration

--- flake.nix
+++ flake.nix
@@ -14,13 +14,31 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
+  let
+    # super simple boilerplate-reducing
+    # lib with a bunch of functions
+    myLib = import ./myLib/default.nix {inherit inputs;};
+  in
+    with myLib; {
+      nixosConfigurations = {
+        # ===================== NixOS Configurations ===================== #
+
+        raytop = mkSystem ./hosts/raytop/configurations/normal.nix;
+        work = mkSystem ./hosts/work/configuration.nix;
+        vps = mkSystem ./hosts/vps/configuration.nix;
+        liveiso = mkSystem ./hosts/liveiso/configuration.nix;
+      };
+
+      homeManagerModules.default = ./homeManagerModules;
+      nixosModules.default = ./nixosModules;
+    };
       {
       nixosConfigurations = {
         "raynix" = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
             ./configuration.nix
-            
+
           ];
         };
         gaming = nixpkgs.lib.nixosSystem {
