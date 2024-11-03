d3135e45f1b207585e874260c0a2fb549ac0c407
MeRay <git@meray.org>
2024-11-03 13:08:13 +0100

    broken home

--- nixosModules/programs/starship.nix
+++ nixosModules/programs/starship.nix
@@ -1,11 +1,46 @@
-{ config, lib, pkgs, ... }: with lib;
-
+# expects option config.shell.which conform to homemanager.
 {
-  environment.systemPackages = with pkgs; [
-    starship
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: let
+  inherit (builtins) hasAttr;
+  inherit (lib) mkIf;
+
+  shellsAvailable = hasAttr "shells" config;
+  
+in {
+  environment.systemPackages = [ pkgs.starship ];
+  fonts.packages = with pkgs; [ (nerdfonts.override { fonts = [ "FiraMono" ]; }) ];
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      format = "$shlvl$shell$username$hostname$nix_shell$git_branch$git_commit$git_state$git_status$directory$jobs$cmd_duration$character";
+      shlvl = {
+        disabled = false;
+        symbol = "ﰬ";
+        style = "bright-red bold";
+      };
+    };
+
+
+
     
-  ];
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraMono" ]; })
-  ];
+  } 
+  // mkIf (shellsAvailable && config.shells.bash.enable)  {
+    enableBashIntegration = true;
+  } // mkIf (shellsAvailable && config.shells.nush.enable)  {
+    enableNushellIntegration = true;
+  } // mkIf (shellsAvailable && config.shells.zsh.enable)  {
+    enableZshIntegration = true;
+  } // mkIf (shellsAvailable && config.shells.fish.enable)  {
+    enableFishIntegration = true;
+  }
+  ;
+
+  
 }
--- nixosModules/users/meray.nix
+++ nixosModules/users/meray.nix
@@ -1,11 +1,18 @@
-{config, pkgs, ...}:
-
 {
+  inputs,
+  ...
+}: let
+  inherit (inputs) home;
+in {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.meray = {
     isNormalUser = true;
     description = "Merlin Raymond";
     extraGroups = [ "networkmanager" "wheel" ];
     #packages = with pkgs; [    ];
+  } // (builtins.hasAttr "home" inputs) {
+    home.username = "meray";
+    home.homeDirectory = "/home/meray";
+
   };
 }
