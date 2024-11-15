4cc18dea32a10090942f82a81d422e76cda8eb9b
MeRay <git@meray.org>
2024-11-15 12:53:04 +0100

    importer returns at least something

--- myLib/importer.nix
+++ myLib/importer.nix
@@ -1,22 +1,20 @@
-dir: importFunction:
+dir: importFunction: lib:
   let
 
-    inherit (builtins) hasSuffix attrNames readDir readFileType isPath;
+    inherit (builtins) attrNames readDir readFileType isPath baseNameOf toString;
+    inherit (lib.strings) hasSuffix removeSuffix;
 
     itemParser = importFunction: {fileODirName, oldPath, oldAttrs}:
       let
-        path = ./. + "${oldPath}/${fileODirName}";
+        path = oldPath + "/${fileODirName}";
 
         isDir = (readFileType path) == "directory";
-        isNix = hasSuffix path ".nix";
+        isNix = hasSuffix (toString path) ".nix";
         isDef = fileODirName == "default.nix";
-
-        
-
 
         parseNix = 
           let
-            name = fileODirName - ".nix";
+            name = removeSuffix ".nix" fileODirName;
             #updateSet
             attrs = oldAttrs.name;
           in
@@ -36,7 +34,7 @@ dir: importFunction:
                   oldPath = path;
                   oldAttrs = attrs; 
                 }
-              ) (attrNames readDir);
+              ) (attrNames (readDir path));
       in
         if !(isPath dir) 
         then throw "importer needs a nix path"
@@ -54,6 +52,6 @@ dir: importFunction:
           )
         );
   in
-    itemParser importFunction { fileODirName = dir; oldPath = ./.; oldAttrs = {};}
+    itemParser importFunction { fileODirName = baseNameOf dir; oldPath = ./.; oldAttrs = {};}
 
-# (import ./importer.nix) ./modules (path: attrs: import path ({a = 2;} // {inherit attrs;}))
+# (import ./importer.nix) ./modules (path: attrs: import path ({a = 2;} // {inherit attrs;})) (import <nixpkgs/lib>) 
