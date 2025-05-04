4c076ac1445e214494cdd450880318471f35ca08
MeRay <git@meray.org>
2025-05-04 17:42:46 +0200

    should really pass paths, or can I read the path after all from each file, through eval getFilePath and then passing to list

--- modules/default.nix
+++ modules/default.nix
@@ -1,12 +1,4 @@
-{
-  lib, pkgs, config, defImp, aPath, id, ...
-}@set: 
-id set {
-  imports = [./wget.nix];
-  config = {};
-  options = {};
-}
-# defImp {
-#   name = "modules";
-#   inherit aPath;
-# }
+{ defImp, ... }@set:
+
+  defImp set ./. "modules"
+
