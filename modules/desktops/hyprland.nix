44b4c06d1988f39ec7ee06d510710bda557020a9
MeRay <git@meray.org>
2025-05-07 03:11:12 +0200

    added foot

--- modules/desktops/hyprland.nix
+++ modules/desktops/hyprland.nix
@@ -16,7 +16,7 @@
       enable = true;
       wayland.enable = true;
   };
-  environment.systemPackages = [ pkgs.hyprpanel ];
+  environment.systemPackages = [ pkgs.hyprpanel pkgs.foot ];
 
   # wayland.windowManager.hyprland = {
   #   enable = true;
