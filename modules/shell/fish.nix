8cc2cc97b946952a77a97ae1735e244a7b0f2f0b
MeRay <git@meray.org>
2025-05-16 01:43:08 +0200

    revert init

--- modules/shell/fish.nix
+++
@@ -1,9 +0,0 @@
-{ 
-  pkgs,
-  ...
-}: {
-  environment.systemPackages = [ pkgs.fish ];
-  users.defaultUserShell = pkgs.fish;
-  programs.fish.enable = true;
-  documentation.man.generateCaches = false;
-}
--- playground/mkEnableTester.nix
+++
@@ -1,9 +0,0 @@
-let
-  lib = import <nixpkgs/lib>;
-  funs = (import ./importerFunctions) {
-    inherit lib;
-    aPath = ["modules"];
-    config = { modules.enable = false;};
-  };
-in
-  ((import ./modules/enabler.nix) funs)
