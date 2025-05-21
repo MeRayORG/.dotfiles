cca615a75e1e7110cf7a92c48368cc0e3ddb3100
MeRay <git@meray.org>
2025-05-21 17:32:36 +0200

    weird error

--- flakes/dev/flake.nix
+++ flakes/dev/flake.nix
@@ -4,24 +4,5 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system: let 
-          pkgs = import nixpkgs {inherit system;};
-          inherit (pkgs) mkShell;
-        in { devShells = {
-
-haskell = mkShell {
-  buildInputs = [       
-      pkgs.haskell.compiler.ghc983
-      pkgs.haskell-language-server
-      pkgs.cabal-install
-      pkgs.hlint
-
-  ];
-};
-
-
-
-
-};});}
+  outputs = { self, nixpkgs, flake-utils }: {};
+}
