50dd82ea535a73c87eb72f49f4108344888332b6
MeRay <git@meray.org>
2025-05-23 19:08:41 +0200

    {232} disable xwayland on cosmic nixpkgs overlay

--- modules/desktops/cosmic.nix
+++ modules/desktops/cosmic.nix
@@ -10,6 +10,13 @@
       desktopManager.cosmic.enable = true;
       displayManager.cosmic-greeter.enable = true;
     };
+    nixpkgs.overlays = [
+      (final: prev: {
+        cosmic-comp = prev.cosmic-comp.override {
+          useXWayland = false;
+        };
+      })
+    ];
     nix.settings = {
       substituters = [ "https://cosmic.cachix.org/" ];
       trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
