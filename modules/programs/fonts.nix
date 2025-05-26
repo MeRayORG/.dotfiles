7b9891d996c2decc1417c7e69c4df0448964f99f
MeRay <git@meray.org>
2025-05-26 10:13:05 +0200

    {239} fonts 2?

--- modules/programs/fonts.nix
+++ modules/programs/fonts.nix
@@ -1,16 +1,35 @@
 { pkgs
 , lib
+, mkHome
 , ... 
-}: {
+}@set: {
   nixpkgs.config.allowUnfree = true;
-  fonts.fontDir.enable = true;
 
-  fonts.packages = [
-    pkgs.corefonts
-    pkgs.fira-code
-    pkgs.noto-fonts
-    pkgs.noto-fonts-extra
-    pkgs.noto-fonts-cjk-sans
-    pkgs.noto-fonts-cjk-serif 
-  ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
-}
+  fonts = {
+    fontDir.enable = true;
+    enableDefaultPackages = true;
+
+    packages = [
+      pkgs.corefonts
+      pkgs.fira-code
+      pkgs.noto-fonts
+      pkgs.noto-fonts-extra
+      pkgs.noto-fonts-cjk-sans
+      pkgs.noto-fonts-cjk-serif 
+      pkgs.ubuntu-sans
+    ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+
+
+    fontconfig = {
+      useEmbeddedBitmaps = true;
+      defaultFonts = {
+        serif = [  "Ubuntu" ];
+        sansSerif = [ "Ubuntu" "Vazirmatn" ];
+        monospace = [ "Ubuntu Mono" ];
+        };
+      };
+  };
+
+} // mkHome set {
+  fonts.fontconfig.enable = true;
+  }
