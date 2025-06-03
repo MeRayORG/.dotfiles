87e37c919b92d26df37b99ddbf8f1c6c9f569555
MeRay <git@meray.org>
2025-06-03 14:15:46 +0200

    {288} its not keyd

--- modules/desktops/kde.nix
+++ modules/desktops/kde.nix
@@ -6,20 +6,12 @@
 { 
   config = lib.mkIf (config.mods.desktops.de == "kde") {
     services = {
+        displayManager.sddm = {
+          wayland.enable = true;
+          enable = true;
+        };
 
-      xserver = {
-
-        # Configure keymap in X11
-        layout = "us";
-        xkbVariant = "alt-intl";
-      };
-
-      # Enable the KDE Plasma Desktop Environment.
-      displayManager.sddm = {
-        enable = true;
-        # wayland.enable = true;
-      };
-      desktopManager.plasma6.enable = true;
+        desktopManager.plasma6.enable = true;
     };
   };
 }
