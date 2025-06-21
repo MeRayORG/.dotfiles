0024c5070a28c74f1ebfc264c1ff277c9d9f76db
MeRay <git@meray.org>
2025-06-21 16:21:10 +0200

    {42} locale overwriting hmgr?

--- hosts/raytop/default.nix
+++ hosts/raytop/default.nix
@@ -138,21 +138,21 @@
   time.timeZone = "Europe/Amsterdam";
 
   # Select internationalisation properties.
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
+  # i18n = {
+  #   defaultLocale = "en_US.UTF-8";
 
-    extraLocaleSettings = {
-      LC_ADDRESS = "nl_NL.UTF-8";
-      LC_IDENTIFICATION = "nl_NL.UTF-8";
-      LC_MEASUREMENT = "nl_NL.UTF-8";
-      LC_MONETARY = "nl_NL.UTF-8";
-      LC_NAME = "nl_NL.UTF-8";
-      LC_NUMERIC = "nl_NL.UTF-8";
-      LC_PAPER = "nl_NL.UTF-8";
-      LC_TELEPHONE = "nl_NL.UTF-8";
-      LC_TIME = "nl_NL.UTF-8";
-    };
-  };
+  #   extraLocaleSettings = {
+  #     LC_ADDRESS = "nl_NL.UTF-8";
+  #     LC_IDENTIFICATION = "nl_NL.UTF-8";
+  #     LC_MEASUREMENT = "nl_NL.UTF-8";
+  #     LC_MONETARY = "nl_NL.UTF-8";
+  #     LC_NAME = "nl_NL.UTF-8";
+  #     LC_NUMERIC = "nl_NL.UTF-8";
+  #     LC_PAPER = "nl_NL.UTF-8";
+  #     LC_TELEPHONE = "nl_NL.UTF-8";
+  #     LC_TIME = "nl_NL.UTF-8";
+  #   };
+  # };
   home-manager.backupFileExtension = "hm-backup";
 
 
