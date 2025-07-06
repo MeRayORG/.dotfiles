c8c3a2a1a0c6c261d9a68e10ba99c80fa8f7ee91
MeRay <git@meray.org>
2025-07-06 15:48:56 +0200

    {53} b4 branch to refactor mkHome

--- modules/programs/yazi.nix
+++ modules/programs/yazi.nix
@@ -4,45 +4,33 @@
 , mkHome
 , ...
 } @set:
-
 {
-
-  config = ({
-    
-    environment.systemPackages = [ 
-      pkgs.yazi
-    ];
-
-
-
-
-  } // mkHome set {
-    programs.yazi = {
-      enable = true;
-      enableZshIntegration = config.mods.shell.zsh.enable;
-      enableNushellIntegration = config.mods.shell.nush.enable;
-      settings = {
-        manager = {
-          show_hidden = true;  # always show dotfiles
-        };
+  environment.systemPackages = [ 
+    pkgs.yazi
+  ];
+} // mkHome set {
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = config.mods.shell.zsh.enable;
+    enableNushellIntegration = config.mods.shell.nush.enable;
+    settings = {
+      manager = {
+        show_hidden = true;  # always show dotfiles
       };
-      keymap.manager.prepend_keymap =
-        let
-          keymapShortcuts = lib.mapAttrsToList (key: cmd: {
-            on = key;
-            run = cmd;
-            desc = "${cmd} (bound to ${key})";
-          });
-        in
-        keymapShortcuts {
-          "i" = "arrow prev";
-          "k" = "arrow next";
-          "j" = "leave";
-          "l" = "enter";
-          "e" = "packing extract";
-          "c" = "packing compress";
-        };
     };
-    
-  });
-}
+    keymap.manager.prepend_keymap = 
+      lib.mapAttrsToList (key: cmd: { on = key; run = cmd;})
+      {
+        "i" = "arrow prev";
+        "k" = "arrow next";
+        "j" = "leave";
+        "l" = "enter";
+        "e" = "packing extract";
+        "z" = "packing compress";
+        "c" = "copy";
+        "x" = "cut";
+        "v" = "paste";
+        "m" = "select --toggle";
+      };
+  };
+} 
