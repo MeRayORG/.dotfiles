7fdef09a7f0adcb500f6a4f80811f21a53d4697e
MeRay <git@meray.org>
2025-09-17 19:19:06 +0200

    improved haskell development flake. DONE

--- flakes/templates/haskell/flake.nix
+++ flakes/templates/haskell/flake.nix
@@ -10,30 +10,45 @@
           pkgs = import nixpkgs {inherit system;};
           
           inherit (pkgs) mkShell lib;
-
-# All packages needed for this project
+############
+# Packages #
+############
 packages = [
   (with pkgs; [
     (with haskell.packages.ghc984; [
-      ghc # GHC compiler in the desired version (will be available on PATH)
+      (ghcWithPackages dependencies) # GHC compiler in the desired version (will be available on PATH)
       ghcid # Continuous terminal Haskell compile checker
       ormolu # Haskell formatter
       hlint # Haskell codestyle checker
       hoogle # Lookup Haskell documentation
-      haskell-language-server # LSP server for editor
+      haskell-language-server# LSP server for editor
       implicit-hie # auto generate LSP hie.yaml file from cabal
       retrie # Haskell refactoring tool
       cabal-install
     ])
   ])
-] ++ lib.mapAttrsToList (pkgs.writeShellScriptBin) scripts
-  ++ totalDeps;
+] ++ lib.mapAttrsToList (pkgs.writeShellScriptBin) scripts;
 
-# Define scripts
+##################
+# Define scripts #
+##################
 scripts = {
 
 } // buildTargetScripts;
 
+
+################
+# Dependencies #
+################
+# must be a derivation
+# add p. if from nixpackages
+dependencies = p: [ 
+  p.composition # for .:
+  ];
+
+#################
+# Build Targets #
+#################
 buildTargets = {
   targetName = rec { # change to the command name you want to launch
     ghc-arg = [];     # arguments to pass to ghc
@@ -45,19 +60,26 @@ buildTargets = {
   };
 };
 
+#############
+# ShellHook #
+#############
+extraShellHook = ''
+
+'';
+
+
 
 buildTargetScripts = 
      lib.mapAttrs' (name: {buildscript, ...}: lib.nameValuePair "build-${name}"  buildscript) buildTargetsOut
   // lib.mapAttrs' (name: {runscript  , ...}: lib.nameValuePair "run-${name}"    runscript  ) buildTargetsOut
   // lib.mapAttrs' (name: {launch     , ...}: lib.nameValuePair name             launch     ) buildTargetsOut;
-buildTargetsOut = lib.mapAttrs targetOut buildTargets;
 
-totalDeps = lib.concatLists ( lib.mapAttrsToList (_: {dependencies? [],...}: dependencies) buildTargets);
+
 
 fallBack = s1: s2: if (s1 != "") then s1 else s2;
 
 
-targetOut = name: {
+buildTargetsOut = lib.mapAttrs (name: {
   ghc-arg  ? [],
   build    ? "",
   run      ? "",
@@ -66,34 +88,38 @@ targetOut = name: {
   root     ? ".",
   ...
 }: rec {
-includes = map (inc: "-i${root +"/"+ inc}") ([""] ++ include);
-ghc-args = map (arg: "-${arg}" ) ghc-arg;
-rm = root + "/" + main;
-buildscript = fallBack build "
-  ghc \"${rm}.hs\" ${lib.concatStringsSep " " (ghc-args ++ includes)} -main-is ${main} -outputdir ${root}/build -o ${rm} &&
-  chmod +x ${rm}.hs 
-";
-runscript = fallBack run "
-  ./${rm}
-";
-launch = "build-${name} && run-${name}";
-hie =
-''
-# BuildTarget: ${name} [${main}] in /${root}
-    - path: "${root}"
-      config:
-        cradle:
-          direct:
-            arguments:
-              - ${lib.concatStringsSep "\n              - " (ghc-args ++ includes)} 
-'';
-};
+
+  ghc-args = map (arg: "-${arg}" ) ghc-arg;
+  includes = map (inc: "-i${root +"/"+ inc}") ([""] ++ include);
+
+  args = ghc-args ++ includes;
+  rm = root + "/" + main;
+
+  buildscript = fallBack build "
+    ghc \"${rm}.hs\" ${lib.concatStringsSep " " args} -main-is ${main} -outputdir ${root}/build -o ${rm} &&
+    chmod +x ${rm}.hs 
+  ";
+  runscript = fallBack run "
+    ./${rm}
+  ";
+  launch = "build-${name} && run-${name}";
+  
+  hie =
+  ''
+  # BuildTarget: ${name} [${main}] in /${root}
+      - path: "${root}"
+        config:
+          cradle:
+            direct:
+              arguments:
+                - ${lib.concatStringsSep "\n              - " args} 
+  '';
+}) buildTargets;
 
 in {
  
 devShell = mkShell {
   inherit packages;
-  LD_LIBRARY_PATH = lib.makeLibraryPath totalDeps;
   shellHook = ''
     ln -sf ${ pkgs.writeText "hie.yaml" 
       ''
@@ -101,8 +127,7 @@ devShell = mkShell {
         multi:
       ${lib.concatStringsSep "\n" (lib.mapAttrsToList (_: {hie,...}: hie) buildTargetsOut)}
       ''} hie.yaml
-    
-
+    ${extraShellHook}
   '';
 };
 
