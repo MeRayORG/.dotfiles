e0672879a270895429e1b57a03208704d6f80428
MeRay <git@meray.org>
2025-09-24 14:31:15 +0200

    {120} wg up 3.0

--- modules/services/wireguard.nix
+++ modules/services/wireguard.nix
@@ -7,11 +7,12 @@
       interfaces = {
         wg0 = {
           ips = [
-            "10.100.0.21/32"
+            "10.100.10.21/32"
           ];
           peers = [
             {
               name = "TX";
+              persistentKeepalive = 25;
               allowedIPs = [
                #"10.0.0.0/16"
                 "10.10.0.0/16"
