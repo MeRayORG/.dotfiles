b8847e4da3c66299b39717bf69027a483e23b590
MeRay <git@meray.org>
2025-11-11 23:26:04 +0100

    {139} back to cosmic to configure

--- hosts/raytop/default.nix
+++ hosts/raytop/default.nix
@@ -82,7 +82,7 @@
       terminal.which = "ghostty";
     };
     flakeDir = "~/nixconf/.";
-    desktops.de = "hypr"; # "hypr" "cosmic" "gnome" "kde" "hyde" "niri" "none"
+    desktops.de = "cosmic"; # "hypr" "cosmic" "gnome" "kde" "hyde" "niri" "none"
 
     services.keyd = {
       mode = {
