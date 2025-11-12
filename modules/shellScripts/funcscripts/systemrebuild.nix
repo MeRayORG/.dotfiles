8b3d672f81124107dc92c7c3e0db3335cea40a94
MeRay <git@meray.org>
2025-11-12 16:13:49 +0100

    sysrebuild update

--- modules/shellScripts/funcscripts/systemrebuild.nix
+++ modules/shellScripts/funcscripts/systemrebuild.nix
@@ -15,7 +15,6 @@ mv /home/meray/.config/gtk-4.0 /home/meray/.config/gtk-4.0.backup
 
 message="$@"
 cd ${config.mods.flakeDir}
-git add -A
 sudo nixos-rebuild switch --flake . --option warn-dirty false &&
 git commit --allow-empty -m "{$(readlink /nix/var/nix/profiles/system | grep -o '[0-9]*')} $message"
 ''
