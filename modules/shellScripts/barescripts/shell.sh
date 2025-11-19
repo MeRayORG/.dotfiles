5453c199f1a45ee81e42597c2fe56f5f229c8b0f
MeRay <git@meray.org>
2025-11-19 15:10:23 +0100

    fixed nix shell scrip

--- modules/shellScripts/barescripts/shell.sh
+++ modules/shellScripts/barescripts/shell.sh
@@ -3,4 +3,4 @@ if [ "$#" -eq 0 ]; then
   exit 1
 fi
 
-NIXPKGS_ALLOW_UNFREE=1 nix shell --impure $(for arg in "$@"; do echo -n "\"nixpkgs#$arg\" "; done)
+NIXPKGS_ALLOW_UNFREE=1 nix shell --impure $(printf "nixpkgs#%s " "$@")
